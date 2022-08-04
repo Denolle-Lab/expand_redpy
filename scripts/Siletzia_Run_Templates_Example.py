8a623c1cf4a3050cbb2a80af68ded80bd2cca208
Nick-Smoczyk <bluescale1999@gmail.com>
2022-08-03 17:17:44 -0700

    starting config and getting more specific volcano metadata

--- scripts/Siletzia_Run_Templates_Example.py
+++ scripts/Siletzia_Run_Templates_Example.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 # import everything
 
 import numpy as np
@@ -12,8 +6,8 @@ from obspy import UTCDateTime
 from obspy.clients.fdsn import Client
 import matplotlib.pyplot as plt
 import h5py
-import sys
-sys.path.append("/data/wsd01/pnwstore/")
+# import sys
+# sys.path.append("/data/wsd01/pnwstore/")
 import eqcorrscan
 from eqcorrscan.core.match_filter import match_filter
 from eqcorrscan.core.match_filter.tribe import Tribe
@@ -23,9 +17,6 @@ import pandas as pd
 from glob import glob
 
 from obspy.core.utcdatetime import UTCDateTime
-
-
-# In[7]:
 
 
 volc_md = pd.read_csv('Volcano_Metadata.csv') # read metadata file to create dataframe of labels
@@ -40,54 +31,22 @@ Rainier_sta = volc_md[volc_md['Volcano_Name'] == 'Mt_Rainier']['Station'].values
 
 #create list of volcanoes
 
-volc_list_names = ['Baker','Hood','St_Helens','Newberry','Rainier'] # list of names of each volcano
-volc_sta = [Baker_sta,Hood_sta,St_Helens_sta,Newberry_sta,Rainier_sta] # lists of stations connected to respective volcanoes
-
-
-# In[8]:
-
-
-#Filter based on distance and then stack all earthquakes in a group to create a template
-
-# Template metadata
-fqmin = 1
-fqmax = 10
-fs=40
-prepick_len = 0.3
-trig_int = 6
-
-homedir = '/home/smocz/redpy_expand_new_files/' #home directory or directory to save new files to
-datatdir = '/home/smocz/2019_data/' #directory to get data from
-
-year = 2019
-
-# tribe = eqcorrscan.core.match_filter.tribe.Tribe(templates = stack_templates)
-
-
-# ### Run the templates over a year - loop through multiple tribes - integrate saving as a csv - Jul 14, 2022
-
-# Save as .py files to run on terminal - divide up into each volcano
-
-# In[ ]:
-
-# ### Running one test template
-
-# Station HOOD on Hood for cluster 17
-
-# In[5]:
+volc_list_names = ['Baker','Hood','Newberry','Rainier','St_Helens'] # list of names of each volcano
+volc_sta = [Baker_sta,Hood_sta,Newberry_sta,Rainier_sta,St_Helens_sta] # lists of stations connected to respective volcanoes
 
 
 
-# In[ ]:
-
+# to replace the volcano loop,
+vv = #read directory
+# vv = 3 #if running multiple volcano scripts at the same times in the same directory, just set for each script
+v = volc_sta[vv]
 
 # loops through julian days
 # run over a year
 
-# for vv,v in enumerate(volc_sta):
-v = Hood_sta
-vv = 1
-for s in range(0,7): #usually 0,len(Hood_sta)
+# for vv,v in enumerate(volc_sta): #can be replaced with v and vv above
+
+for s in range(11,len(v)): #usually 0,len(v)
     station = v[s]
     print(station)
     print(volc_list_names[vv])
@@ -98,7 +57,7 @@ for s in range(0,7): #usually 0,len(Hood_sta)
         print('No Tribe for Station')
         continue
     try:
-        obspy.read(glob(datatdir+str(year)+'/*/'+station+'.*.'+str(year)+'.*')[0])
+        obspy.read(glob(f'{datadir}{year}/*/{station}/{station}.*.{year}.*')[0]).select(channel=chan)
     except:
         print('No Data for Station')
         continue
@@ -109,7 +68,7 @@ for s in range(0,7): #usually 0,len(Hood_sta)
     for i in range(1,366): # normally 1,366
         parties = []
         t0=time()
-        st = obspy.read(*glob(datatdir+str(i).zfill(3)+'/'+station+'.*.'+str(year)+'.*'))
+        st = obspy.read(*glob(f'{datadir}{year}/*/{sta}/{sta}.*.{year}.{str(i).zfill(3)}')).select(channel=chan)
         st.detrend(type='demean')
         st.resample(fs)
         st.filter(type='bandpass',freqmin=fqmin,freqmax=fqmax)
@@ -122,10 +81,10 @@ for s in range(0,7): #usually 0,len(Hood_sta)
             T.templates[ii].prepick = prepick_len 
         if len(st)==0: continue
         try:
-            party = T.detect(stream=st,starttime=st[0].stats.starttime,endtime=st[0].stats.endtime,threshold=0.6, threshold_type='absolute',xcorr_func = 'fmf',trig_int=trig_int, plot=False, return_stream=False, ignore_bad_data=True,overlap='calculate')
+            party = T.detect(stream=st,starttime=st[0].stats.starttime,endtime=st[0].stats.endtime,threshold=thr, threshold_type=thr_t,xcorr_func = xfunc,trig_int=trig_int, plot=plot, return_stream=r_st, ignore_bad_data=i_b_d,overlap=overlap)
         except:
             continue
-        party.decluster(metric='avg_cor',trig_int=trig_int) #had to add trig_int, it is minimum detection separation in seconds
+        party.decluster(metric=metric,trig_int=trig_int) #had to add trig_int, it is minimum detection separation in seconds
         t2=time()
         print("it tooks %2f s to launch the party" %(t2-t1))
         print(party)
@@ -149,3 +108,7 @@ for s in range(0,7): #usually 0,len(Hood_sta)
 
 
 # In[ ]:
+
+
+
+
